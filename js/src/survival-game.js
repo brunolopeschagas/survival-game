d8c42dc534eb2253aad59f25aa26eb516b33f6c4
brunolopeschagas <brunolopeschagas@gmail.com>
2024-11-09 13:30:17 -0300

    player load

--- js/src/survival-game.js
+++ js/src/survival-game.js
@@ -1,10 +1,12 @@
+import MainScene from "./scenes/MainScene.js";
+
 const config = {
     width: 512,
     height: 512,
     backgroundColor: '#333333',
     type: Phaser.AUTO,
     parent: 'survival-game',
-    scene:[],
+    scene:[MainScene],
     scale:{
         zoom:2,
     },
